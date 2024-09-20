1de5d55ef97592fbf428655cccee8cf55042f73e
Hassan-7866 <khanhassanali7866@gmail.com>
2024-09-20 17:07:20 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Workflow from "./components/Workflow";
 import FeatureSection from "./components/FeatureSection";
-import HeroSection from "./components/Herosection";
+import HeroSection from "./components/HeroSection";
 import Navbar from "./components/navbar";
 import Pricing from "./components/Pricing";
 import Testimonials from "./components/Testimonials";
@@ -10,8 +10,9 @@ const App = () => {
     <>
       <Navbar />
       <div className="max-w-7xl mx-auto pt-20 px-6">
-        <HeroSection />
+      
         <FeatureSection />
+        <HeroSection />
         <Workflow />
         <Pricing />
         <Testimonials />
